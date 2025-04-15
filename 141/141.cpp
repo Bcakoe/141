3e6286fc148665f0f51318194a84905238540bdc
Jenia <Jenia@gmail.com>
2025-04-15 12:30:12 +0300

    Сделал изменения в проекте

---
+++ 141/141.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int al,bl;
+	cout<<"¬ведите первое число: "<<endl;
+	cin>>al;
+	cout<<"¬ведите второе число: "<<endl;
+	cin>>bl;
+	cout<<"—умма чисел: "<<al+bl<<endl;
+	return 0;
+}
